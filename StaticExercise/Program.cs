88b3835b7b5c0aba58e122ddec49215ca4dde570
Travis Gersch <t_gersch@hotmail.com>
2022-07-15 14:43:58 -0500

    static exercise 1 finished

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using StaticExercise;
+
+
+
+
+var celsius1 = TempConverter.FahrenheitToCelsius(0);
+var celsius2 = TempConverter.FahrenheitToCelsius(100);
+
+Console.WriteLine("0 degrees and 100 degrees Fahrenheit converted to Celsius are respectively:\n");
+Console.WriteLine(celsius1);
+Console.WriteLine(celsius2);
+
+
+var fahrenheit1 = TempConverter.CelsiusToFahrenheit(0);
+var fahrenheit2 = TempConverter.CelsiusToFahrenheit(100);
+
+Console.WriteLine();
+Console.WriteLine("Here we convert the same values of 0 degrees and 100 degrees Celsius to Fahrenhet, respectively:\n");
+Console.WriteLine(fahrenheit1);
+Console.WriteLine(fahrenheit2);
+
+Console.WriteLine();
+Console.WriteLine("Or...Fahrenheit to Celsius (50)\n");
+Console.WriteLine(TempConverter.FahrenheitToCelsius(50));
+Console.WriteLine("\nAnd Celsius to Fahrenheit (50)\n");
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(50));
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise;
+
+public static class TempConverter
+{
+
+
+
+
+    public static double FahrenheitToCelsius(double number)
+    {
+        var result = (number - 32) / 1.8;
+        return result;
+    }
+
+
+
+    public static double CelsiusToFahrenheit(double number)
+    {
+        var result = (number * 9) / 5 + 32;
+        return result;
+    }
+
+
+
+
+
+
+
+}
